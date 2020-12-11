8c45bb2a7f909c03556803192e0b057e12934945
asd15 <akashsdesai15@gmail.com>
2020-12-12 01:31:01 +0530

    Authentication

---
+++ auth/lib/src/domain/signup_service_contract.dart
@@ -0,0 +1,7 @@
+import 'package:async/async.dart';
+
+import 'token.dart';
+
+abstract class ISignUpService {
+  Future<Result<Token>> signUp(String name, String email, String password);
+}
---
+++ lib/composition_root.dart
@@ -0,0 +1,56 @@
+import 'package:auth/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:serviceproviderapp/cache/local_store.dart';
+//import 'package:serviceproviderapp/fake_restaurant_api.dart';
+import 'package:serviceproviderapp/states_management/auth/auth_cubit.dart';
+//import 'package:serviceproviderapp/states_management/helpers/header_cubit.dart';
+//import 'package:serviceproviderapp/states_management/restaurant/restaurant_cubit.dart';
+import 'package:serviceproviderapp/ui/pages/auth/auth_page.dart';
+//import 'package:serviceproviderapp/ui/pages/home/home_page_adapter.dart';
+import 'package:http/http.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'cache/local_store_contract.dart';
+//import 'ui/pages/home/restaurant_list_page.dart';
+
+class CompositionRoot {
+  static SharedPreferences _sharedPreferences;
+  static ILocalStore _localStore;
+  static String _baseUrl;
+  static Client _client;
+
+  static configure() {
+    _localStore = LocalStore(_sharedPreferences);
+    _client = Client();
+    _baseUrl = "http://localhost:3000";
+  }
+
+  static Widget composeAuthUi() {
+    IAuthApi _api = AuthApi(_baseUrl, _client);
+    AuthManager _manger = AuthManager(_api);
+    AuthCubit _authCubit = AuthCubit(_localStore);
+    ISignUpService _signupService = SignUpService(_api);
+
+    return BlocProvider(
+      create: (BuildContext context) => _authCubit,
+      child: AuthPage(_manger, _signupService),
+    );
+  }
+
+//  static Widget composeHomeUi() {
+//    FakeRestaurantApi _api = FakeRestaurantApi(50);
+//    RestaurantCubit _restaurantCubit =
+//        RestaurantCubit(_api, defaultPageSize: 20);
+//    IHomePageAdapter adapter = HomePageAdapter(_restaurantCubit);
+//
+//    return MultiCubitProvider(providers: [
+//      CubitProvider<RestaurantCubit>(
+//        create: (BuildContext context) => _restaurantCubit,
+//      ),
+//      CubitProvider<HeaderCubit>(
+//        create: (BuildContext context) => HeaderCubit(),
+//      )
+//    ], child: RestaurantListPage(adapter));
+//  }
+}
